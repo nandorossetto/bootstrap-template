14ec1945653174511053ee4879854aba47c7c048
Fernando <nandorossetto@gmail.com>
2014-12-15 15:38:17 -0200

    add load grunt task in the project

--- Gruntfile.js
+++ Gruntfile.js
@@ -66,11 +66,7 @@ module.exports = function(grunt) {
 
     });
 
-    grunt.loadNpmTasks('grunt-contrib-compass');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+    require('load-grunt-tasks')(grunt);
 
     //Building file to deploy
     grunt.registerTask('deploy', ['compass', 'concat', 'uglify']);
